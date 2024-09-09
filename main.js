a1b1859d6b6a7f756a8b4bbcc5924da837741f06
mohi035 <mohammadmohiuddinhmc@gmail.com>
2024-09-09 22:42:48 +0600

    class17 commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text",{
+    String:["Frontend Developer", "Backend Developer", "Digital marketer"],
+    Typespeed:100,
+    Backspeed:100,
+    BackDelay:100,
+    loop:true 
+});
